22c9bf6adea93824864ee81b20301658b8e59d78
Mike Vorozhbyanskiy <vorojbanskiy@yamoney.ru>
2016-02-10 15:41:44 +0300

    added habrahabr

--- geektimes/geektimes_adv_remover.js
+++ geektimes/geektimes_adv_remover.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Geektimes adv remover
 // @namespace    https://github.com/mgfox/userscripts/
-// @version      0.2
+// @version      0.3
 // @description  remove ads from geektimes.ru site
 // @author       Mike VV
 // @match        http*://geektimes.ru/*
+// @match        http*://habrahabr.ru/*
 // @updateURL		https://raw.githubusercontent.com/mgfox/userscripts/master/geektimes/geektimes_adv_remover.js
 // @downloadURL		https://raw.githubusercontent.com/mgfox/userscripts/master/geektimes/geektimes_adv_remover.js
 // @grant        none
