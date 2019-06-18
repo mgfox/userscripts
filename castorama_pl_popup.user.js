afe8c1dda05d4636939a5311332afc07b39d8093
Mike <mgfox@ya.ru>
2019-06-18 22:27:05 +0200

    Update castorama_pl_popup.user.js

--- castorama_pl_popup.user.js
+++ castorama_pl_popup.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         block annoying newspaper popup
+// @name         Block Flashing Stuff
 // @namespace    http://mgfox.github.io
 // @version      0.1
 // @description  try to take over the world!
@@ -10,7 +10,12 @@
 
 (function() {
     'use strict';
-        var elem = document.querySelector('.newspapper-popup');
-        elem.style.display = "none";
-        console.log("newspapper popup blocked ...");
-})();
+    if (location.href.search(/castorama.pl\/*/i) > -1) {
+        console.log("castorama.pl detected ...");
+        (function(){
+            var elem = document.querySelector('.newspapper-popup');
+            elem.style.display = "none";
+            console.log("#newspapper-popup eliminated.");
+        })();
+    }
+})();    
