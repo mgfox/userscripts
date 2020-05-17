6c3ffb1f422b2ca41ddb788a454b2150f353eaec
Mike <mgfox@ya.ru>
2020-05-17 12:53:05 +0200

    Update remove_video_banner.user.js

--- fextralife/remove_video_banner.user.js
+++ fextralife/remove_video_banner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       	Video Banner Remover
 // @namespace  	https://github.com/mgfox/userscripts/
-// @version    	0.1.3
+// @version    	0.1.4
 // @description This script removes video banner from fextralife wiki
 // @include     https://*.wiki.fextralife.com/*
 // @grant       none
@@ -14,11 +14,15 @@ if (location.href.search("wiki.fextralife.com") > -1) {
   var func = function(){
     var removed = 0
     // remove by ID 
-    for (elemId in ["menu-add-a", "menu-add-b", "video-stream-container"]) {
+    for (elemId in ["menu-add-a", "menu-add-b", "video-stream-container", "sidebar-wrapper", "sidebar-nav"]) {
+      console.log("Userscript: Checking for id=" + elemId + " ...")
       var e = document.getElementById(elemId)
       if (e != null) {
         e.remove()
         removed = 1
+        console.log("Userscript: Element id=" + elemId + " removed")
+      } else {
+        console.log("Userscript: Element id=" + elemId + " not found")
       }
     }
     for (className in ["ad-sidebar"]) {
